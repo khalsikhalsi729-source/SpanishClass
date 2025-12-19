899b130c82679100664ccf75c0d32c05e86aa2c9
wdwedqwdkhlbvdgwedyieh <At412014@gmail.com>
2025-12-19 11:42:24 +0100

    button app  problem

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
-// config.js
 const CLIENT_CONFIG = {
     endpoint: 'https://cloud.appwrite.io/v1', 
-    projectId: '694312f00019cf598ee4', //⚠️ (نسيت ما صورتيش ليا Project ID، جيبو من الصفحة الرئيسية ديال Appwrite)
-    databaseId: '694313050004e60fb48f', // ✅ هذا هو ID الحقيقي ديالك من الصورة
-    collectionId: 'students' // ✅ حسب الصورة ID هو "students"
+    projectId: '694312f00019cf598ee4',
+    databaseId: '694313050004e60fb48f',
+    collectionId: 'students'
 };
 
 const { Client, Databases, Query } = Appwrite;
