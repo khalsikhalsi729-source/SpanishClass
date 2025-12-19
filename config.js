ce751006a234ebdbd480178df6444b0abebe8da9
wdwedqwdkhlbvdgwedyieh <At412014@gmail.com>
2025-12-19 11:47:09 +0100

    back to the first

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+// config.js
 const CLIENT_CONFIG = {
     endpoint: 'https://cloud.appwrite.io/v1', 
-    projectId: '694312f00019cf598ee4',
-    databaseId: '694313050004e60fb48f',
-    collectionId: 'students'
+    projectId: '694312f00019cf598ee4', //⚠️ (نسيت ما صورتيش ليا Project ID، جيبو من الصفحة الرئيسية ديال Appwrite)
+    databaseId: '694313050004e60fb48f', // ✅ هذا هو ID الحقيقي ديالك من الصورة
+    collectionId: 'students' // ✅ حسب الصورة ID هو "students"
 };
 
 const { Client, Databases, Query } = Appwrite;
